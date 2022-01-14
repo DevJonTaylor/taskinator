310e54cdb491e4f561fc476c0b5a6e9b4e95df89
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-14 10:32:48 -0600

    Retrieving text and selector from form to create the task.  Resets the form to accept another one.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,34 @@ class AbstractElement {
     return document.querySelector(selector);
   }
 
-  newEl(tagName, {innerText = '', cls = []}) {
+  #forEachEl(arrToBe, cb) {
+    if(!Array.isArray(arrToBe)) arrToBe = [arrToBe];
+    for(let node of arrToBe) cb(node);
+  }
+
+  newEl(tagName, {innerText = '', cls = [], children = []}) {
     const el = document.createElement(tagName);
     if(innerText) el.innerText = innerText;
-    if(cls.length) {
-      if(!Array.isArray(cls)) cls = [cls];
-      el.classList.add(...cls);
-    }
-
+    this.#forEachEl(cls, c => el.classList.add(c));
+    this.#forEachEl(children, child => el.appendChild(child));
+    
     return el;
   }
 
-  li(options = {innerText: '', cls: []}) {
-    return this.newEl('li', options);
+  li({innerText = '', cls = [], children = []}) {
+    return this.newEl('li', {innerText, cls, children});
+  }
+
+  span({innerText = '', cls = [], children = []}) {
+    return this.newEl('span', {innerText, cls, children});
+  }
+
+  h3({innerText = '', cls = [], children = []}) {
+    return this.newEl('h3', {innerText, cls, children});
+  }
+
+  div({innerText = '', cls = [], children = []}) {
+    return this.newEl('div', {innerText, cls, children});
   }
 }
 
@@ -25,11 +40,14 @@ class TaskFormController extends AbstractElement {
     super();
     this.form = this.$('.task-form');
     this.taskName = this.$('[name="task-name"]');
-    this.taskType = this.$('.task-type');
+    this.taskType = this.$('[name="task-type"]');
     this.submitBtn = this.$('#save-task')
   }
 
-  
+  reset() {
+    this.taskName.value = '';
+    this.taskType.value = '';
+  }
 }
 
 class TaskListController extends AbstractElement {
@@ -39,7 +57,12 @@ class TaskListController extends AbstractElement {
   }
 
   newTask(taskText, taskType) {
-    return this.li({innerText: taskText, cls:'task-item'})
+    const divChildren = [
+      this.h3({ innerText: taskText, cls: 'task-name' }),
+      this.span({ innerText: taskType, cls: 'task-type' })
+    ];
+    const div = this.div({ cls: 'task-info', children: divChildren })
+    return this.li({ cls:'task-item', children: div });
   }
 
   appendToList(...li) {
@@ -53,8 +76,9 @@ const taskList = new TaskListController;
 function newTaskHandler(event) {
   event.preventDefault();
 
-  const task = taskList.newTask(taskForm.taskName.value);
+  const task = taskList.newTask(taskForm.taskName.value, taskForm.taskType.value);
   taskList.appendToList(task);
+  taskForm.reset();
 }
 
 taskForm.form.addEventListener('submit', newTaskHandler);
