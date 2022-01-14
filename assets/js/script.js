9ab1a6fda908f35a56c1ecc66d433b7ac4783769
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-14 09:18:46 -0600

    Created a form event that adds a task to the list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,60 @@
-var btn = document.body.querySelector('#save-task');
+class AbstractElement {
+  $(selector) {
+    return document.querySelector(selector);
+  }
 
-function createTaskElement(taskText) {
-  var li = document.createElement('li');
-  li.innerText = taskText;
-  li.classList.add('task-item');
-  return li;
+  newEl(tagName, {innerText = '', cls = []}) {
+    const el = document.createElement(tagName);
+    if(innerText) el.innerText = innerText;
+    if(cls.length) {
+      if(!Array.isArray(cls)) cls = [cls];
+      el.classList.add(...cls);
+    }
+
+    return el;
+  }
+
+  li(options = {innerText: '', cls: []}) {
+    return this.newEl('li', options);
+  }
 }
 
-function addNewTask(event) {
-  var list = document.querySelector('#tasks-to-do');
-  var task = createTaskElement('Things to do.');
+class TaskFormController extends AbstractElement {
+  
+  constructor() {
+    super();
+    this.form = this.$('.task-form');
+    this.taskName = this.$('[name="task-name"]');
+    this.taskType = this.$('.task-type');
+    this.submitBtn = this.$('#save-task')
+  }
 
-  list.appendChild(task);
+  
 }
 
-btn.addEventListener('click', addNewTask);
+class TaskListController extends AbstractElement {
+  constructor() {
+    super();
+    this.list = this.$('.task-list');
+  }
+
+  newTask(taskText, taskType) {
+    return this.li({innerText: taskText, cls:'task-item'})
+  }
+
+  appendToList(...li) {
+    this.list.append(...li);
+  }
+}
+
+const taskForm = new TaskFormController;
+const taskList = new TaskListController;
+
+function newTaskHandler(event) {
+  event.preventDefault();
+
+  const task = taskList.newTask(taskForm.taskName.value);
+  taskList.appendToList(task);
+}
+
+taskForm.form.addEventListener('submit', newTaskHandler);
